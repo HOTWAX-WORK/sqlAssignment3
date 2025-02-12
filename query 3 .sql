8390a2cbb7a4e8d95b79152eee76845a5c34f7b1
Kratika-nenwani <Kratika-nenwani>
2025-02-12 14:35:45 +0530

    4 commit

--- query 3 .sql
+++ query 3 .sql
@@ -5,7 +5,9 @@ select
 from
 	return_item ri
 join return_header rh on
-	rh.RETURN_ID = ri.RETURN_ID
+	rh.RETURN_ID = ri.RETURN_ID and 
+    MONTH(rh.ENTRY_DATE ) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
+    AND YEAR(rh.ENTRY_DATE) = YEAR(CURRENT_DATE)
 join person on
 	rh.FROM_PARTY_ID = person.PARTY_ID
 group by
