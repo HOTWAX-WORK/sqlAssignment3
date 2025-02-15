4897775c766cf2942dc9c5c485be8d642ed3d911
Kratika-nenwani <Kratika-nenwani>
2025-02-15 21:00:17 +0530

    update commit

--- query 12.sql
+++ query 12.sql
@@ -2,14 +2,15 @@ SELECT
     distinct os.ORDER_ID,
     oh.ORDER_DATE ,
     oh.STATUS_ID as ORDER_STATUS,
-    S.ORIGIN_FACILITY_ID 
+    S.ORIGIN_FACILITY_ID ,
+    ofc.CHANGE_DATETIME 
 FROM
     order_shipment os
 LEFT JOIN
     picklist_bin pb ON os.ORDER_ID = pb.primary_order_id
 left join order_header oh on oh.ORDER_ID = os.ORDER_ID 
 left join shipment s on S.SHIPMENT_ID =OS.SHIPMENT_ID 
+left join order_facility_change ofc on ofc.ORDER_ID = os.ORDER_ID 
 WHERE
     pb.primary_order_id IS null
-order by os.ORDER_ID 
-
+order by os.ORDER_ID
