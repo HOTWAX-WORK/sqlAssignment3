1b4e9812427d2811338f246beb3168d63b58e3e7
Kratika Nenwani <142520394+Kratika-nenwani@users.noreply.github.com>
2025-03-04 09:08:02 +0530

    Update query 9 .sql

--- query 9 .sql
+++ query 9 .sql
@@ -1,3 +1,14 @@
+-- 9 Total Facilities That Sell the Product
+-- Business Problem:
+-- Retailers want to see how many (and which) facilities (stores, warehouses, virtual sites) currently offer a product for sale.
+
+-- Fields to Retrieve:
+
+-- PRODUCT_ID
+-- PRODUCT_NAME (or INTERNAL_NAME)
+-- FACILITY_COUNT (number of facilities selling the product)
+-- (Optionally) a list of FACILITY_IDs if more detail is needed
+
 select
 	p.PRODUCT_ID,
 	p.PRODUCT_NAME,
