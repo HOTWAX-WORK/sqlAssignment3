2b0e3cf626d63b69b91e5e868e9a672a2682d5de
Kratika Nenwani <142520394+Kratika-nenwani@users.noreply.github.com>
2025-03-04 09:05:31 +0530

    Update query 4 .sql

--- query 4 .sql
+++ query 4 .sql
@@ -1,3 +1,14 @@
+-- 4 Returns and Appeasements
+-- Business Problem:
+-- The retailer needs the total amount of items, were returned as well as how many appeasements were issued.
+
+-- Fields to Retrieve:
+
+-- TOTAL RETURNS
+-- RETURN $ TOTAL
+-- TOTAL APPEASEMENTS
+-- APPEASEMENTS $ TOTAL
+
 select
 	count(ri.RETURN_ITEM_SEQ_ID),
 	sum(ri.RETURN_PRICE*ri.RETURN_QUANTITY) as RETURN_TOTAL,
