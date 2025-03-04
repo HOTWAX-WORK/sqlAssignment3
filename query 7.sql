e8e7fb4b85d92a340171741b6b5d712c435968ed
Kratika Nenwani <142520394+Kratika-nenwani@users.noreply.github.com>
2025-03-04 09:07:03 +0530

    Update query 7.sql

--- query 7.sql
+++ query 7.sql
@@ -1,3 +1,14 @@
+-- 7 Store with Most One-Day Shipped Orders (Last Month)
+-- Business Problem:
+-- Identify which facility (store) handled the highest volume of “one-day shipping” orders in the previous month, useful for operational benchmarking.
+
+-- Fields to Retrieve:
+
+-- FACILITY_ID
+-- FACILITY_NAME
+-- TOTAL_ONE_DAY_SHIP_ORDERS
+-- REPORTING_PERIOD
+
 select
 	COUNT(oisg.SHIPMENT_METHOD_TYPE_ID) as TOTAL_ONE_DAY_SHIP_ORDERS,
 	oisg.FACILITY_ID,
