70621cd120c6c4b4488e69cc2bd86204437549b6
Kratika Nenwani <142520394+Kratika-nenwani@users.noreply.github.com>
2025-03-04 09:09:20 +0530

    Update query 12.sql

--- query 12.sql
+++ query 12.sql
@@ -1,3 +1,15 @@
+-- 12 Orders Without Picklist
+-- Business Problem:
+-- A picklist is necessary for warehouse staff to gather items. Orders missing a picklist might be delayed and need attention.
+
+-- Fields to Retrieve:
+
+-- ORDER_ID
+-- ORDER_DATE
+-- ORDER_STATUS
+-- FACILITY_ID
+-- DURATION (How long has the order been assigned at the facility)
+
 SELECT
     distinct os.ORDER_ID,
     oh.ORDER_DATE ,
